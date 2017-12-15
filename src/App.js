39925d10588ace888d81f5787b16828250351ed9
Kevin Springer <KevinUSCU@outlook.com>
2017-12-15 13:44:18 -0800

    Add basic Navbar and Toolbar (toolbar is not dynamic yet)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar'
+import Toolbar from './components/Toolbar'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <Navbar />
+        <Toolbar />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-default">
+        <div className="navbar-header">
+          <a className="navbar-brand">React Inbox</a>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
