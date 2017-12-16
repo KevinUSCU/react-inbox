f318d7efa53f29103ff7ac746cc57d3f4f038682
Kevin Springer <KevinUSCU@outlook.com>
2017-12-16 14:58:03 -0800

    Bring in messages from App and pass to Message component.

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Message from './Message'
 
-function MessageList() {
-  return <Message />
+function MessageList({ messages }) {
+  return messages.map((el, i) => <Message key={ i } message={ el } />)
 }
 
 export default MessageList
