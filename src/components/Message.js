f84e606358966a1a0df7676d116c911edc461e9f
Kevin Springer <KevinUSCU@outlook.com>
2017-12-16 14:59:17 -0800

    Bring in message prop and use its contents to populate message attributes.

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,33 +2,30 @@ import React from 'react'
 import CheckBox from './CheckBox'
 import Star from './Star'
 
-const isSelected = true
-const isRead = false
-const isStarred = false
-const messageText = 'Here is some message text that has a bunch of stuff - yeah!'
 function checkFunction () { console.log('Check toggle!') }
 function starFunction () { console.log('Star toggle!') }
 function selectMessageFunction () { console.log('Click on message!')}
 
-function Message() {
+function Message({ message }) {
+  const { selected, starred, labels, read, subject } = message
   return (
     <div className={ 
-        isSelected ? (isRead ? "row message read selected" : "row message unread selected")
-                  : (isRead ? "row message read" : "row message unread")
+        selected ? (read ? "row message read selected" : "row message unread selected")
+                  : (read ? "row message read" : "row message unread")
       } onClick={ selectMessageFunction }>
       <div className="col-xs-1">
         <div className="row">
           <div className="col-xs-2">
-            <CheckBox isChecked={ isSelected } clickFunction={ checkFunction } />
+            <CheckBox isChecked={ selected } clickFunction={ checkFunction } />
           </div>
           <div className="col-xs-2">
-            <Star isStarred={ isStarred } clickFunction={ starFunction } />
+            <Star isStarred={ starred } clickFunction={ starFunction } />
           </div>
         </div>
       </div>
       <div className="col-xs-11">
         <a>
-          { messageText }
+          { subject }
         </a>
       </div>
     </div>
