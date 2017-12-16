1590cfc4d877990235f3970e3601bb654fa55f37
Kevin Springer <KevinUSCU@outlook.com>
2017-12-16 13:40:35 -0800

    Update navbar alignment with row and col.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,15 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-default">
-        <div className="navbar-header">
-          <a className="navbar-brand">React Inbox</a>
-        </div>
-    </nav>
+    <div className="row">
+      <div className="col">
+        <nav className="navbar navbar-default">
+          <div className="navbar-header">
+            <a className="navbar-brand">React Inbox</a>
+          </div>
+        </nav>
+      </div>
+    </div>
   )
 }
 
