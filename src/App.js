2875785bddbf535b6a5f39d33a9b25f74a320cc8
Kevin Springer <KevinUSCU@outlook.com>
2017-12-17 02:56:07 -0800

    Add functions to page controls.

--- src/App.js
+++ src/App.js
@@ -16,34 +16,109 @@ class App extends Component {
 
   // TOOLBAR FUNCTIONS
   toggleSelectAllMessages = () => {
-    console.log('Messages selected!!!')
+    this.setState((prevState) => {
+      const allMessagesSelected = (prevState.messages.filter(el => el.selected).length === prevState.messages.length)
+      const newMessages = prevState.messages.map(el => {
+        allMessagesSelected ? el.selected = false : el.selected = true
+        return el
+      })
+      return { messages: newMessages }
+    })
   }
 
-  applyLabel = () => {
-    console.log('Label Applied!!!')
+  applyLabel = (event) => {
+    const label = event.target.value
+    if (label) { // ignores cases where the selection header is selected
+      this.setState((prevState) => {
+        const newMessages = prevState.messages.map(el => {
+          if (el.selected) {
+            if (!el.labels.includes(label)) {
+              const newLabels = [ ...el.labels, label ]
+              el.labels = newLabels
+            } 
+          }
+          return el
+        })
+        return { messages: newMessages }
+      })
+    }
   }
 
-  removeLabel = () => {
-    console.log('Label Removed!!!')
+  removeLabel = (event) => {
+    const label = event.target.value
+    if (label) { // ignores cases where the selection header is selected
+      this.setState((prevState) => {
+        const newMessages = prevState.messages.map(el => {
+          if (el.selected) {
+            const newLabels = el.labels.filter(el => el !== label)
+            el.labels = newLabels 
+          }
+          return el
+        })
+        return { messages: newMessages }
+      })
+    }
   }
 
   deleteMessages = () => {
-    console.log('Deleted!!!')
+    this.setState((prevState) => {
+      const newMessages = prevState.messages.filter(el => !el.selected)
+      return { messages: newMessages }
+    })
   }
 
   markRead = () => {
-    console.log('Marked Read!!!')
+    this.setState((prevState) => {
+      const newMessages = prevState.messages.map(el => {
+        if (el.selected) el.read = true
+        return el
+      })
+      return { messages: newMessages }
+    })
   }
 
   markUnread = () => {
-    console.log('Marked Unread!!!')
+    this.setState((prevState) => {
+      const newMessages = prevState.messages.map(el => {
+        if (el.selected) el.read = false
+        return el
+      })
+      return { messages: newMessages }
+    })
   }
 
   // MESSAGELIST FUNCTIONS
-  toggleCheckBox () { console.log('Check toggle!') }
-  toggleStar () { console.log('Star toggle!') }
-  selectMessage () { console.log('Click on message!')}
+  toggleCheckBox = (event) => { 
+    const messageId = Number(event.target.closest('.message').id)
+    this.setState((prevState) => {
+      const newMessages = [ ...prevState.messages ]
+      const index = newMessages.findIndex(el => el.id === messageId)
+      newMessages[index].selected = !newMessages[index].selected
+      return { messages: newMessages }
+    })
+  }
 
+  toggleStar = (event) => {
+    const messageId = Number(event.target.closest('.message').id)
+    this.setState((prevState) => {
+      const newMessages = [ ...prevState.messages ]
+      const index = newMessages.findIndex(el => el.id === messageId)
+      newMessages[index].starred = !newMessages[index].starred
+      return { messages: newMessages }
+    })
+  }
+
+  selectMessage = (event) => {
+    const messageId = Number(event.target.closest('.message').id)
+    this.setState((prevState) => {
+      const newMessages = [ ...prevState.messages ]
+      const index = newMessages.findIndex(el => el.id === messageId)
+      newMessages[index].read = true
+      return { messages: newMessages }
+    })
+  }
+
+  // RENDER
   render() {
     return (
       <div className="container">
