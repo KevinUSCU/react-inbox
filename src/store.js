c0df9207551b96a2f83778abf0f5a4c5af4cea8a
Kevin Springer <KevinUSCU@outlook.com>
2018-02-20 15:54:28 -0800

    Add basic store and reducer.

---
+++ src/store.js
@@ -0,0 +1,18 @@
+import thunk from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './reducers'
+import logger from 'redux-logger'
+
+const initialState = {}
+const middleware = [
+  thunk,
+  logger
+]
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  applyMiddleware(...middleware)
+)
+
+export default store
