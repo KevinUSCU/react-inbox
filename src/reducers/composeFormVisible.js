45967355832fd31e7aaa7c46e733b1b17583620c
Kevin Springer <KevinUSCU@outlook.com>
2018-02-25 14:17:41 -0800

    Add composeFormVisibile reducer.

---
+++ src/reducers/composeFormVisible.js
@@ -0,0 +1,7 @@
+const intitialState = false
+
+export default(state = intitialState, action) => {
+  switch(action.type) {
+    default: return state
+  }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
+import composeFormVisible from './composeFormVisible'
 import labelOptions from './labelOptions'
 import messageList from './messageList'
 import selectedMessages from './selectedMessages'
 
 export default combineReducers({
+  composeFormVisible,
   labelOptions,
   messageList,
   selectedMessages
