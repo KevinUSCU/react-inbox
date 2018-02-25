a25b809439c66ef2b6f03ea70d833fc60aeaae51
Kevin Springer <KevinUSCU@outlook.com>
2018-02-25 14:12:05 -0800

    Add labelOptions reducer.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
+import labelOptions from './labelOptions'
 import messageList from './messageList'
 import selectedMessages from './selectedMessages'
 
 export default combineReducers({
+  labelOptions,
   messageList,
   selectedMessages
 })
